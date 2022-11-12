860a22317a8af7705af2658ffd51f47b8a581b48
Cezary Bujak <cezarybujak28@gmail.com>
2022-11-12 23:04:17 +0100

    Styling and user homepage - major changes
    + Bootstrap styling
    + HTML files refactorization and reorganization
    + Added new user homepage
    + Added main site
    + Added links to user homepage components

--- website/views.py
+++ website/views.py
@@ -5,6 +5,30 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
+def mainPage():
+    return render_template("base.html")
+
+@views.route('/home')
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/calendar')
+@login_required
+def calendar():
+    return render_template("calendar.html", user=current_user)
+
+@views.route('/schedule')
+@login_required
+def schedule():
+    return render_template("schedule.html", user=current_user)
+
+@views.route('/customers')
+@login_required
+def customers():
+    return render_template("customers.html", user=current_user)
+
+@views.route('/statistics')
+@login_required
+def statistics():
+    return render_template("statistics.html", user=current_user)
